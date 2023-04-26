460f5e704aa21cff40438dffeed159796e2c7adf
Matvii Butenov <matvej1888@gmail.com>
2023-04-26 17:06:40 +0200

    commit with Externalizable

--- src/com/company/Serializer.java
+++ src/com/company/Serializer.java
@@ -18,23 +18,17 @@ public class Serializer {
 
     public static <T extends Externalizable> T deserialize(T object, String fileName) throws IOException, ClassNotFoundException {
         File file = new File(fileName);
-        System.out.println("file");
         if (!file.exists()) {
             file.createNewFile();
-            System.out.println("new file");
 
         }
         FileInputStream fileInputStream = new FileInputStream(file);
-        System.out.println("file input stream");
         if (file.length() == 0) {
-            System.out.println("no file new instance");
             fileInputStream.close();
             return object;
         } else {
             ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
-            System.out.println("object input stream");
             object.readExternal(objectInputStream);
-            System.out.println("new object with data");
             objectInputStream.close();
             fileInputStream.close();
             return object;
